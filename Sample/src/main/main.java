f60e9b524e4625304a91e822ea4c94e310894ade
nuchi <syo.hashidate@gmail.com>
2018-06-28 23:22:08 +0900

    Add files via upload

---
+++ Sample/src/main/main.java
@@ -0,0 +1,43 @@
+package main;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class main extends JFrame{
+    String prefix = "[テスト]";
+    public static void main(String args[]) {
+
+        main frame = new main("タイトル");
+        frame.setVisible(true);
+
+    }
+
+    main(String title) {
+        setTitle(title);
+        setBounds(100, 100, 400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon icon = new ImageIcon("./img/icon.png");
+        setIconImage(icon.getImage());
+
+        JPanel p = new JPanel();
+
+        JLabel lab1 = new JLabel(prefix + "下記の物はプレフィックスです。");
+        JLabel lab2 = new JLabel();
+
+        p.add(lab1);
+        p.add(lab2);
+
+        Container contentPane =getContentPane();
+        contentPane.add(p, BorderLayout.CENTER);
+
+
+    }
+}
+
+
+
+
